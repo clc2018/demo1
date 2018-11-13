ac3807e79b2e341feff8f2da1428e0ad0dd1f0d5
JX User <jxuser@pingworks.de>
2018-11-13 12:26:37 +0100

    basement for superfeature

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO: superfeature
+
 @SpringBootApplication
 public class DemoApplication {
 
