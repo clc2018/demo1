3e8ff0b23a3d65eb91c4172295349445962d0f2f
JX User <jxuser@pingworks.de>
2018-11-13 12:26:40 +0100

    next

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //my-feature
+//the next
 @SpringBootApplication
 public class DemoApplication {
 
