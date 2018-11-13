1d2bb1b7c62f5fb5ad71de63080cf9e390a98ce7
JX User <jxuser@pingworks.de>
2018-11-13 12:18:08 +0100

    i was here

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//my-feature
 @SpringBootApplication
 public class DemoApplication {
 
